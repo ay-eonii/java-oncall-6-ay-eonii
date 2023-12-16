5a539b1e555e36e27a1902acfe1c047df4510e50
ay-eonii <naayen@naver.com>
2023-12-16 16:37:43 +0900

    feat(OutputView): 비상 근무표 출력하기

--- src/main/java/oncall/view/OutputView.java
+++ src/main/java/oncall/view/OutputView.java
@@ -1,8 +1,22 @@
 package oncall.view;
 
+import java.util.List;
+
 public class OutputView {
+
+    private static final String FORMAT = "%s %s\n";
 
     public void printExceptionMessage(String message) {
         System.out.println(message);
+    }
+
+    public void printOnCall(List<String> date, List<String> onCall) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < date.size(); i++) {
+            stringBuilder.append(String.format(FORMAT, date.get(i), onCall.get(i)));
+        }
+
+        System.out.println(stringBuilder);
     }
 }
