75e7f1d43be6b0b28c04e996c37bf5b305859456
ay-eonii <naayen@naver.com>
2023-12-16 16:58:14 +0900

    feat(OnCall): 인원 수 검사

--- src/main/java/oncall/domain/OnCall.java
+++ src/main/java/oncall/domain/OnCall.java
@@ -1,12 +1,21 @@
 package oncall.domain;
 
+import oncall.view.constant.ExceptionMessage;
+
 import java.util.Queue;
 
 public abstract class OnCall {
     private final Queue<Member> order;
 
     public OnCall(Queue<Member> order) {
+        validate(order);
         this.order = order;
+    }
+
+    private void validate(Queue<Member> order) {
+        if (order.size() < Range.MIN.value || order.size() > Range.MAX.value) {
+            throw new IllegalArgumentException(ExceptionMessage.INVALID_MEMBER_COUNT);
+        }
     }
 
     public Member getNextOnCall() {
@@ -21,5 +30,15 @@ public abstract class OnCall {
             return "";
         }
         return order.peek().toString();
+    }
+
+    private enum Range {
+        MIN(5), MAX(35);
+
+        private final int value;
+
+        Range(int value) {
+            this.value = value;
+        }
     }
 }
